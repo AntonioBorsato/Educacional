92a73164f7d8d1b1d247a5c6fff7782d3e794a1b
AntonioBorsato <antonioborsato16@gmail.com>
2024-11-12 19:50:35 -0300

    Implementação do Crud de professor e do Swagger

--- educacional/src/main/java/grupointegrado/com/educacional/aluno/controller/AlunoController.java
+++ educacional/src/main/java/grupointegrado/com/educacional/aluno/controller/AlunoController.java
@@ -4,6 +4,7 @@ import grupointegrado.com.educacional.aluno.model.Aluno;
 import grupointegrado.com.educacional.aluno.dto.AlunoRequestDTO;
 import grupointegrado.com.educacional.aluno.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +17,28 @@ public class AlunoController {
     private AlunoRepository repository;
 
     @GetMapping
-    public List<Aluno> findAll() {
-        return this.repository.findAll();
+    public ResponseEntity<List<Aluno>> findAll() {
+        return ResponseEntity.ok(this.repository.findAll());
     }
 
     @GetMapping("/{id}")
-    public Aluno findById(@PathVariable Integer id) {
-        return this.repository.findById(id)
+    public ResponseEntity<Aluno> findById(@PathVariable Integer id) {
+        Aluno aluno = this.repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado com o ID especificado"));
+
+        return ResponseEntity.ok(aluno);
     }
 
     @GetMapping("/matricula/{matricula}")
-    public Aluno findByMatricula(@PathVariable String matricula) {
-        return this.repository.findByMatricula(matricula)
+    public ResponseEntity<Aluno> findByMatricula(@PathVariable String matricula) {
+        Aluno aluno = this.repository.findByMatricula(matricula)
                 .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado com a matrícula especificada"));
+
+        return ResponseEntity.ok(aluno);
     }
 
     @PostMapping
-    public Aluno save(@RequestBody AlunoRequestDTO dto) {
+    public ResponseEntity<Aluno> save(@RequestBody AlunoRequestDTO dto) {
         if (this.repository.findByMatricula(dto.matricula()).isPresent()) {
             throw new IllegalArgumentException("O aluno não pode ser criado: já existe um aluno com o número de matrícula " + dto.matricula());
         }
@@ -44,12 +49,11 @@ public class AlunoController {
         aluno.setMatricula(dto.matricula());
         aluno.setData_nascimento(dto.data_nascimento());
 
-        return this.repository.save(aluno);
+        return ResponseEntity.ok(this.repository.save(aluno));
     }
 
     @PutMapping("/{id}")
-    public Aluno update(@PathVariable Integer id,
-                        @RequestBody AlunoRequestDTO dto) {
+    public ResponseEntity<Aluno> update(@PathVariable Integer id, @RequestBody AlunoRequestDTO dto) {
         Aluno aluno = this.repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado com o ID especificado"));
 
@@ -58,15 +62,15 @@ public class AlunoController {
         aluno.setMatricula(dto.matricula());
         aluno.setData_nascimento(dto.data_nascimento());
 
-        return this.repository.save(aluno);
+        return ResponseEntity.ok(this.repository.save(aluno));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Integer id) {
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         Aluno aluno = this.repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado com o ID especificado"));
 
         this.repository.delete(aluno);
+        return ResponseEntity.noContent().build();
     }
-
 }
